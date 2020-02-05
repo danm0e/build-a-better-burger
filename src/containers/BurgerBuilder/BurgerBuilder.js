71fdd80f85c428d627a0f5a0e5c4cce665ade439
Dan Moe <dan.moe@telegraph.co.uk>
2020-02-05 21:50:31 +0000

    add more/less ingredient functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
@@ -9,14 +16,65 @@ class BurgerBuilder extends Component {
 			bacon: 0,
 			cheese: 0,
 			meat: 0
+		},
+		totalPrice: 4
+	}
+
+	addHandler = type => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceAddition = PRICES[type]
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceAddition;
+
+		this.setState({ 
+			totalPrice: newPrice, 
+			ingredients: updatedIngredients 
+		})
+	}
+
+	removeHandler = type => {
+		const oldCount = this.state.ingredients[type];
+		if(oldCount <= 0 ){
+			return;
 		}
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceDeduction = PRICES[type]
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+
+		this.setState({ 
+			totalPrice: newPrice, 
+			ingredients: updatedIngredients 
+		})
 	}
 
 	render() {
+		const disabledInfo = {
+			...this.state.ingredients
+		}
+
+		for( let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0
+		}
+
 		return (
 			<>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls />
+				<BuildControls 
+					addHandler={this.addHandler}
+					removeHandler={this.removeHandler}
+					disabled={disabledInfo}
+					currentPrice={this.state.totalPrice}
+				/>
 			</>
 		);
 	}
